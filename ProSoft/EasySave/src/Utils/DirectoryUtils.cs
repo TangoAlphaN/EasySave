b82f2e952d6f7b59f7fb6043eaf2932946b596de
Louison <louison.leidinger@gmail.com>
2023-02-08 17:13:57 +0100

    Add utils class

---
+++ ProSoft/EasySave/src/Utils/DirectoryUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.src.Utils
+{
+    public static class DirectoryUtils
+    {
+        public static void CopyDirectory(String src, String dest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void CopyFile(String src, String dest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProSoft/EasySave/src/Utils/LogUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.src.Utils
+{
+    public static class LogUtils
+    {
+        private static string _logFile;
+
+        public static void LogError(String message)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static void LogSave(Save save)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void LogTransfer(String dest, String src)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int CalculateTransferTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
