00c061e6e9a9d3313861b81ab7ad091f9825e83f
arnoux23u <arnouxguillaume54@hotmail.fr>
2023-02-09 00:58:44 +0100

    update main

---
+++ ProSoft/EasySave/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EasySave.src.Render;
+
+namespace EasySave.src
+{
+    internal class Program
+    {
+        protected Program()
+        {
+        }
+
+        protected static void Main(string[] args)
+        {
+            View v = new View();
+            v.Start();
+        }
+    }
+}
+
+
