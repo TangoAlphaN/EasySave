06ab059e633d0053d33a47bb12b52c3d4ba38cf3
N.PARISSE <parissenathalie@orange.fr>
2023-02-12 19:21:55 +0100

    set up a multi-page feature in an application using the MVVM model

--- ProSoft/EasySave/src/Render/Views/MainWindow.xaml.cs
+++ ProSoft/EasySave/src/Render/Views/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EasySave.src;
 
 namespace EasySave
 {
@@ -27,6 +28,7 @@ namespace EasySave
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModel();
         }
 
         private void Creat_MouseDown(object sender, MouseButtonEventArgs e)
--- ProSoft/EasySave/src/ViewModel.cs
+++ ProSoft/EasySave/src/ViewModel.cs
@@ -2,6 +2,9 @@
 using EasySave.src.Utils;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.Input;
+
 
 namespace EasySave.src
 {
@@ -10,6 +13,20 @@ namespace EasySave.src
     /// </summary>
     public class ViewModel
     {
+        
+        private ICommand GoToPageHomeCommand { get; set; }
+        
+        private object CurrentPage { get; set; }
+
+        public ViewModel()
+        {
+            GoToPageHomeCommand = new RelayCommand(GoToHomePage);
+        }
+
+        private void GoToHomePage()
+        {
+            CurrentPage = new HomePage();
+        }
 
         /// <summary>
         /// Create save method
