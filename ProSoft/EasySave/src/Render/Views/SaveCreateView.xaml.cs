762ab75ac716ce91483ccac206a96dc42bc6c184
N.PARISSE <parissenathalie@orange.fr>
2023-02-13 21:51:57 +0100

    CreateSave corrections

--- ProSoft/EasySave/src/Render/Views/SaveCreateView.xaml.cs
+++ ProSoft/EasySave/src/Render/Views/SaveCreateView.xaml.cs
@@ -19,7 +19,7 @@ namespace EasySave.src.Render.Views
     public partial class SaveCreateView : UserControl
     {
         SaveType _type;
-        ViewModel _vm = new ViewModel();
+
         private void SelectFilePathCommand(object sender, RoutedEventArgs e)
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
@@ -28,29 +28,34 @@ namespace EasySave.src.Render.Views
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
                 TxtSrc.Text = dialog.FileName;
         }
-        
+
         private void SelectFilePathCommandDest(object sender, RoutedEventArgs e)
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
             dialog.InitialDirectory = "C:\\";
             dialog.IsFolderPicker = true;
-            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
                 TxtDest.Text = dialog.FileName;
         }
 
         private void RadioCheck(Object sender, EventArgs e)
         {
-            if(btnFull.IsChecked == true)
+            if (btnFull.IsChecked == true)
                 _type = Full;
-            if(btnDiff.IsChecked == true)
+            if (btnDiff.IsChecked == true)
                 _type = Differential;
         }
 
         public void CreateNewSave(Object sender, RoutedEventArgs routedEventArgs)
         {
-            _vm.CreateSave(CreatSaveName.Text, TxtSrc.Text, TxtDest.Text, _type);
+            CreateSave();
         }
-        
+
+        private Save CreateSave()
+        {
+            return Save.CreateSave(CreatSaveName.Text, TxtSrc.Text, TxtDest.Text, _type);
+        }
+
         public SaveCreateView()
         {
             InitializeComponent();
