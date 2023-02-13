3cc69d056ee307afaa9ef0919beee614d8300c3e
arnoux23u <arnouxguillaume54@hotmail.fr>
2023-02-13 10:52:49 +0100

    settings

---
+++ ProSoft/EasySave/src/ViewModels/SettingsChangeLanguageViewModel.cs
@@ -0,0 +1,36 @@
+﻿
+using EasySave.Properties;
+using EasySave.src.Models;
+using EasySave.src.Render;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace EasySave.src.ViewModels
+{
+    public class SettingsChangeLanguageViewModel : INotifyPropertyChanged
+    {
+        private CollectionViewSource SettingsItemsCollection;
+        public ICollectionView HomeSourceCollection => SettingsItemsCollection.View;
+
+        public SettingsChangeLanguageViewModel()
+        {
+            ObservableCollection<HomeItems> homeItems = new ObservableCollection<HomeItems>
+            {
+                 new HomeItems { HomeName = $"{Resource.Lang_fr_FR}", HomeImage = @"Assets/Lang_Icon.png" },
+                 new HomeItems { HomeName = $"{Resource.Lang_en_US}", HomeImage = @"Assets/notepad_icon.png" },
+                 new HomeItems { HomeName = $"{Resource.Lang_it_IT}", HomeImage = @"Assets/notepad_icon.png" },
+                 new HomeItems { HomeName = $"{Resource.Lang_ru_RU}", HomeImage = @"Assets/notepad_icon.png" },
+            };
+
+            SettingsItemsCollection = new CollectionViewSource { Source = homeItems };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+}
--- ProSoft/EasySave/src/ViewModels/SettingsChangeLogsFormatViewModel.cs
+++ ProSoft/EasySave/src/ViewModels/SettingsChangeLogsFormatViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using EasySave.Properties;
 using EasySave.src.Models;
 using EasySave.src.Render;
 using System.Collections.ObjectModel;
@@ -7,16 +8,19 @@ using System.Windows.Data;
 
 namespace EasySave.src.ViewModels
 {
-    public class SettingsViewModel : INotifyPropertyChanged
+    public class SettingsChangeLogsFormatViewModel : INotifyPropertyChanged
     {
         private CollectionViewSource SettingsItemsCollection;
         public ICollectionView HomeSourceCollection => SettingsItemsCollection.View;
 
-        public SettingsViewModel()
+        public SettingsChangeLogsFormatViewModel()
         {
             ObservableCollection<HomeItems> homeItems = new ObservableCollection<HomeItems>
             {
-
+                 new HomeItems { HomeName = $"{Resource.Lang_fr_FR}", HomeImage = @"Assets/Lang_Icon.png" },
+                 new HomeItems { HomeName = $"{Resource.Lang_en_US}", HomeImage = @"Assets/notepad_icon.png" },
+                 new HomeItems { HomeName = $"{Resource.Lang_it_IT}", HomeImage = @"Assets/notepad_icon.png" },
+                 new HomeItems { HomeName = $"{Resource.Lang_ru_RU}", HomeImage = @"Assets/notepad_icon.png" },
             };
 
             SettingsItemsCollection = new CollectionViewSource { Source = homeItems };
