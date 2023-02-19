e92edfcea4b263007fbe49b86cd578404c41b2b5
arnoux23u <arnouxguillaume54@hotmail.fr>
2023-02-19 20:08:06 +0100

    main windows

---
+++ ProSoft/EasyClient/Views/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Interfaces;
+
+namespace EasyClient.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INavigationWindow
+    {
+        public ViewModel ViewModel
+        {
+            get;
+        }
+
+        public MainWindow(ViewModel viewModel, IPageService pageService, INavigationService navigationService)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+            SetPageService(pageService);
+
+            navigationService.SetNavigationControl(RootNavigation);
+        }
+
+        #region INavigationWindow methods
+
+        public Frame GetFrame()
+            => RootFrame;
+
+        public INavigation GetNavigation()
+            => RootNavigation;
+
+        public bool Navigate(Type pageType)
+            => RootNavigation.Navigate(pageType);
+
+        public void SetPageService(IPageService pageService)
+            => RootNavigation.PageService = pageService;
+
+        public void ShowWindow()
+            => Show();
+
+        public void CloseWindow()
+            => Close();
+
+        #endregion INavigationWindow methods
+
+        /// <summary>
+        /// Raises the closed event.
+        /// </summary>
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Make sure that closing this window will begin the process of closing the application.
+            Application.Current.Shutdown();
+        }
+    }
+}
