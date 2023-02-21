6172d29cf85c99f1c760c524a7bc9c3fb7cf4d0b
Alexandre <alexandre.perreux@viacesi.fr>
2023-02-21 11:23:46 +0100

    patch notifications + start software package

---
+++ ProSoft/EasySave/src/Utils/NotificationUtils.cs
@@ -0,0 +1,59 @@
+﻿using Notification.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace EasySave.src.Utils
+{
+    public static class NotificationUtils
+    {
+
+        /// <summary>
+        /// Send a notification with multiple parameters
+        /// </summary>
+        public static void SendNotification(string title, string message, NotificationType type = NotificationType.Error, string url = null, int time = 5)
+        {
+            new NotificationManager().Show(new NotificationContent
+            {
+                Title = title,
+                Message = message,
+                Type = type,
+            },
+            onClick: () => OpenUrl(url, url.Length > 0),
+            expirationTime: TimeSpan.FromSeconds(time)
+            );
+        }
+
+        private static void OpenUrl(string url, bool open = false)
+        {
+            if (!open) return;
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
